b264de6e1e90d9377950e8ad2c53d8f54494a85e
jacob <jbutler8@terpmail.umd.edu>
2018-02-08 17:52:33 -0800

    update piece inheritance

--- display.rb
+++ display.rb
@@ -1,43 +1,60 @@
-require_relative 'board'
-require_relative 'cursor'
-require 'byebug'
-require 'colorize'
+require "colorize"
+require_relative "cursor"
 
 class Display
+
+  attr_reader :board, :notifications, :cursor
+
   def initialize(board)
     @board = board
     @cursor = Cursor.new([0, 0], board)
+    @notifications = {}
+  end
+
+  def build_grid
+    @board.rows.map.with_index do |row, i|
+      build_row(row, i)
+    end
+  end
+
+  def build_row(row, i)
+    row.map.with_index do |piece, j|
+      color_options = colors_for(i, j)
+      piece.to_s.colorize(color_options)
+    end
+  end
+
+  def colors_for(i, j)
+    if cursor.cursor_pos == [i, j] && cursor.selected
+      bg = :light_green
+    elsif cursor.cursor_pos == [i, j]
+      bg = :light_red
+    elsif (i + j).odd?
+      bg = :light_blue
+    else
+      bg = :light_yellow
+    end
+    { background: bg }
+  end
+
+  def reset!
+    @notifications.delete(:error)
+  end
+
+  def uncheck!
+    @notifications.delete(:check)
+  end
+
+  def set_check!
+    @notifications[:check] = "Check!"
   end
 
   def render
-    puts "  #{(0..7).to_a.join(" ")}"
-    @board.grid.each_with_index do |row, i|
-      row.each_with_index do |col, j|
-        print "#{i}" if j == 0
+    system("clear")
+    puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
+    build_grid.each { |row| puts row.join }
 
-        if @cursor.cursor_pos == [i, j]
-          print " #{col}".colorize(background: :light_blue)
-        else
-          print " #{col}"
-        end
-        
-        puts "" if j == 7
-      end
+    @notifications.each do |_key, val|
+      puts val
     end
   end
-
-  def move_cursor
-    while true
-      @cursor.get_input
-      render
-    end
-  end
-
-end
-
-if __FILE__ == $PROGRAM_NAME
-  board = Board.new
-  display = Display.new(board)
-  display.render
-  display.move_cursor
-end
--- piece.rb
+++ piece.rb
@@ -1,5 +1,13 @@
-require_relative 'board'
-require 'singleton'
+require_relative 'pieces/piece'
+require_relative 'pieces/bishop'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
+require_relative 'pieces/null'
+require_relative 'pieces/stepable'
+require_relative 'pieces/slideable'
 
 class Piece
 
