6817b15b3bfde1b16173c0a5dea58058378b2695
jacob <jbutler8@terpmail.umd.edu>
2018-02-10 10:31:01 -0800

    human player class added. game class added

--- board.rb
+++ board.rb
@@ -1,53 +1,129 @@
-require_relative 'piece'
-require_relative 'cursor'
-
-class MovementError < StandardError
-  def start_message
-    "No piece to move."
-  end
-
-  def end_message
-    "Cannot move here."
-  end
-end
-
+require_relative 'pieces'
 
 class Board
+  attr_reader :rows
 
-  attr_accessor :grid
-
-  def initialize
-    @grid = Array.new(8) { Array.new(8) }
-    @grid.each_index do |row|
-      @grid[row].each_index do |col|
-        @grid[row][col] = Piece.new if row <= 1 || row >= 6
-      end
-    end
-  end
-
-  def move_piece(start_pos, end_pos)
-    start_row, start_col = start_pos
-    end_row, end_col = end_pos
-
-    raise MovementError if self[start_pos].nil?
-    raise MovementError if !in_bounds?(end_pos)
-
-    self[end_pos] = self[start_pos]
-    self[start_pos] = nil  # nil piece
-  end
-
-  def in_bounds?(pos)
-    (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) ? false : true
+  def initialize(fill_board = true)
+    @sentinel = NullPiece.instance
+    make_starting_grid(fill_board)
   end
 
   def [](pos)
+    raise 'invalid pos' unless valid_pos?(pos)
+
     row, col = pos
-    @grid[row][col]
+    @rows[row][col]
   end
 
-  def []=(pos, value)
+  def []=(pos, piece)
+    raise 'invalid pos' unless valid_pos?(pos)
+
     row, col = pos
-    @grid[row][col] = value
+    @rows[row][col] = piece
+  end
+
+  def add_piece(piece, pos)
+    raise 'position not empty' unless empty?(pos)
+
+    self[pos] = piece
+  end
+
+  def checkmate?(color)
+    return false unless in_check?(color)
+
+    pieces.select { |p| p.color == color }.all? do |piece|
+      piece.valid_moves.empty?
+    end
+  end
+
+  def dup
+    new_board = Board.new(false)
+
+    pieces.each do |piece|
+      piece.class.new(piece.color, new_board, piece.pos)
+    end
+
+    new_board
+  end
+
+  def empty?(pos)
+    self[pos].empty?
+  end
+
+  def in_check?(color)
+    king_pos = find_king(color).pos
+    pieces.any? do |p|
+      p.color != color && p.moves.include?(king_pos)
+    end
+  end
+
+  def move_piece(turn_color, start_pos, end_pos)
+    raise 'start position is empty' if empty?(start_pos)
+
+    piece = self[start_pos]
+    if piece.color != turn_color
+      raise 'You must move your own piece'
+    elsif !piece.moves.include?(end_pos)
+      raise 'Piece does not move like that'
+    elsif !piece.valid_moves.include?(end_pos)
+      raise 'You cannot move into check'
+    end
+
+    move_piece!(start_pos, end_pos)
+  end
+
+  # move without performing checks
+  def move_piece!(start_pos, end_pos)
+    piece = self[start_pos]
+    raise 'piece cannot move like that' unless piece.moves.include?(end_pos)
+
+    self[end_pos] = piece
+    self[start_pos] = sentinel
+    piece.pos = end_pos
+
+    nil
+  end
+
+  def pieces
+    @rows.flatten.reject(&:empty?)
+  end
+
+  def valid_pos?(pos)
+    pos.all? { |coord| coord.between?(0, 7) }
+  end
+
+  private
+
+  attr_reader :sentinel
+
+  def fill_back_row(color)
+    back_pieces = [
+      Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+    ]
+
+    i = color == :white ? 7 : 0
+    back_pieces.each_with_index do |piece_class, j|
+      piece_class.new(color, self, [i, j])
+    end
+  end
+
+  def fill_pawns_row(color)
+    i = color == :white ? 6 : 1
+    8.times { |j| Pawn.new(color, self, [i, j]) }
+  end
+
+  def find_king(color)
+    king_pos = pieces.find { |p| p.color == color && p.is_a?(King) }
+    king_pos || (raise 'king not found?')
+  end
+
+  def make_starting_grid(fill_board)
+    @rows = Array.new(8) { Array.new(8, sentinel) }
+    return unless fill_board
+    %i(white black).each do |color|
+      fill_back_row(color)
+      fill_pawns_row(color)
+    end
   end
 
 end
